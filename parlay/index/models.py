89f2cc0252b0ee12a224451ad3b2bd4d63512d12
gaijinsupertrapper <gredbla@gmail.com>
2018-02-21 17:03:38 +0600

    style

--- parlay/index/models.py
+++ parlay/index/models.py
@@ -9,7 +9,7 @@ class Book(models.Model):
     title = models.CharField(max_length = 150, default = r'Yet another book')
     author = models.CharField(max_length = 100, default = r'By yet another author')
     release = models.DateField()
-    description = models.CharField(max_length = 500, default = r'Description for the book')
+    description = models.TextField(default = r'Description for the book')
     def __str__(self):
         return self.title
 
